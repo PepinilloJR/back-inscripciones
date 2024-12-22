0ddf346f2f927bc77f7fd8a7ce9d197d8e282080
Harumi236 <harumi236@proton.me>
2024-12-22 16:12:03 -0300

    Agregada URL de inscripciones por materia

--- lateapp/urls.py
+++ lateapp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path("inscripciones/", views.InscripcionTardiaBulkCreateView.as_view(), name="bulk_create_inscripciones"),
     path("cursos/", views.CursoBulkCreateView.as_view(), name="bulk_create_cursos"),
 
-    path('materia/distribute/<int:materia_id>/', views.DistributeAlumnosView.as_view(), name='distribute_alumnos'),
-    path('materia/optimize/<int:materia_id>/', views.OptimizeDistributionView.as_view(), name='optimize_distribution'),
+    path('materias/inscripciones/<int:materia_nombre>/', views.InscripcionesPorMateriaView.as_view(), name='inscripciones_por_materia'),
+
+    path('materia/distribute/<int:materia_nombre>/', views.DistributeAlumnosView.as_view(), name='distribute_alumnos'),
+    path('materia/optimize/<int:materia_nombre>/', views.OptimizeDistributionView.as_view(), name='optimize_distribution'),
 ]
