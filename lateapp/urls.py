b43afcacf9c091dad90f826cc9d7ce1cd1c2b3cb
Harumi236 <harumi236@proton.me>
2025-01-14 08:44:34 -0300

    Cambios en urls para inscripciones por materia

--- lateapp/urls.py
+++ lateapp/urls.py
@@ -12,6 +12,12 @@ urlpatterns = [
     path("inscripciones/<int:pk>/", views.InscripcionTardiaDetailView.as_view(), name="inscripcion-detail"),
     path("inscripciones/bulk", views.InscripcionTardiaBulkCreateView.as_view(), name="bulk_create_inscripciones"),
 
+    # Inscripciones por materia, algoritmo de distribución y algoritmo de optimización
+    path('inscripciones/materia/<int:materia_id/', views.InscripcionesPorMateriaView.as_view(), name='inscripciones_por_materia'),
+    path('inscripciones/materia/distribute/<int:materia_id>/', views.DistributeAlumnosView.as_view(), name='distribute_alumnos'),
+    path('inscripciones/materia/optimize/<int:materia_id>/', views.OptimizeDistributionView.as_view(), name='optimize_distribution'),
+
+
     path("cursos/", views.CursoListCreateView.as_view(), name="curso-list-create"),
     path("cursos/<int:pk>/", views.CursoDetailView.as_view(), name="curso-detail"),
     path("cursos/bulk", views.CursoBulkCreateView.as_view(), name="bulk_create_cursos"),
@@ -21,8 +27,6 @@ urlpatterns = [
 
     path('materias/', views.MateriaListCreateView.as_view(), name='materia-list-create'),
     path('materias/<int:pk>/', views.MateriaDetailView.as_view(), name='materia-detail'),
-    path('materias/inscripciones/<int:materia_nombre>/', views.InscripcionesPorMateriaView.as_view(), name='inscripciones_por_materia'),
 
-    path('materia/distribute/<int:materia_nombre>/', views.DistributeAlumnosView.as_view(), name='distribute_alumnos'),
-    path('materia/optimize/<int:materia_nombre>/', views.OptimizeDistributionView.as_view(), name='optimize_distribution'),
+
 ]
